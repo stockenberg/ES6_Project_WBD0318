85b55b5c7ff6e125efdc098e3991f3ebbf7fcb58
Leon Buchner <Leon.buchner@live.de>
2018-09-19 15:45:49 +0200

    added basic notification layout => next step js function

--- app/classes/Notification.js
+++ app/classes/Notification.js
@@ -9,3 +9,20 @@ class Notification{
     }
 
 }
+
+/**
+<= Notification trigger =>
+- wenn post created
+- wenn post geupdated
+- wenn post bearbeitet/gelöscht wird
+
+Notification wird aufgerufen in den jewieligen Dateien
+
+Vorgehensweise:
+
+- Design für Notification wie bei Useproof mit Animation
+- Ausblenden nach 5 Sekunden oder durch schließen Knopf
+- Auf Display Block setzen bei den Triggern
+- Je nach Trigger den Text ändern
+
+**/
