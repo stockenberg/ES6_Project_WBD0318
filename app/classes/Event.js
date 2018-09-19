99bc0c3c51a01eb80effa87e436ee4d88f7bd51d
Leon Buchner <Leon.buchner@live.de>
2018-09-19 14:45:09 +0200

    event improvements

--- app/classes/Event.js
+++ app/classes/Event.js
@@ -5,6 +5,7 @@ class Event {
 
     init(){
     }
+
     //Get Element by ID
     id(identifier) {
         return document.getElementById(identifier);
@@ -13,100 +14,23 @@ class Event {
     class(classname) {
         return document.getElementsByClassName(classname);
     }
-    //onsubmit Event
-    submit() {
-        object.onsubmit = function() {
-            //do something
-        }
+    //Get Elements By Tagname
+    tag(tagname) {
+        return document.getElementsByTagName(tagname);
     }
-     status(){
-        if(navigator.onLine) {
-            //true or false
-        }
-        else {
-            //true or false
-        }
+    //Get Form by Name
+    form(formname){
+        return document.forms[formname];
     }
 }
 
 let event = new Event();
 event.init();
 
-console.log(event.id("test"));
-console.log(event.class("card-title"));
+//console.log(event.id("test"));
+//console.log(event.class("card-title"));
 
 //event.id("id"); === document.getElementByID("id");
 //event.class("classname") === document.getElementsByClassName(classname);
-
-/**List of all JS ES6 Events
-
-    offline
-    onabort
-    onafterprint
-    onbeforeonload
-    onbeforeprint
-    onblur
-    oncanplay
-    oncanplaythrough
-    onchange
-    onclick
-    oncontextmenu
-    ondblclick
-    ondrag
-    ondragend
-    ondragenter
-    ondragleave
-    ondragover
-    ondragstart
-    ondrop
-    ondurationchange
-    onemptied
-    onended
-    onerror
-    onfocus
-    onformchange
-    onforminput
-    onhaschange
-    oninput
-    oninvalid
-    onkeydown
-    onkeypress
-    onkeyup
-    onload
-    onloadeddata	
-    onloadedmetadata
-    onloadstart
-    onmessage
-    onmousedown
-    onmousemove
-    onmouseout
-    onmouseover
-    onmouseup
-    onmousewheel
-    onoffline
-    ononline
-    onpagehide
-    onpageshow
-    onpause
-    onplay
-    onplaying
-    onpopstate
-    onprogress
-    onratechange
-    onreadystatechange
-    onredo
-    onresize
-    onscroll
-    onseeked
-    onseeking
-    onselect
-    onstalled
-    onstorage
-    onsubmit
-    onsuspend
-    ontimeupdate
-    onundo
-    onunload
-    onvolumechange
-    onwaiting
-    **/
+//event.tag("tagname") === document.getElementsByTagName(tagname);
+//event.form("formname");
